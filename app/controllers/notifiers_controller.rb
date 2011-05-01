d553eee71f78437f84a4554f01feba955a4f3d40
logan henriquez <logan@henriquez.net>
2011-05-01 11:14:26 -0700

    removing keys from history

--- app/controllers/notifiers_controller.rb
+++
@@ -1,8 +0,0 @@
-class NotifiersController < ApplicationController
-  
-  def show
-    @task = "Build a demo of an app that drops in notifications by #{Time.now}"
-    Notifier.notify(current_user)
-  end
-  
-end
