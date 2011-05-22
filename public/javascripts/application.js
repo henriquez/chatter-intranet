258c1cc06187bf6f9f9b02425247f594292885b2
logan henriquez <logan@henriquez.net>
2011-05-22 16:21:27 -0700

    added jquery ui and text area expander - working

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,25 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// Every jQuery function you want to run at render time must be inside the jQuery call
+jQuery(function() {
+
+    // text areas should auto-expand
+	$('textarea#post-text-area').TextAreaExpander(60, 1300);
+
+
+ 	// Styling calls
+	$('button').button();
+});
+
+
+
+
+// **************************************************
+// lower level functions that don't run unless called
+// **************************************************
+
+
+
+
+
