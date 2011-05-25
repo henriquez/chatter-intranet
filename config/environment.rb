80ed730098a1b21e922cdc5bb7ae6505d8cd9cfb
logan henriquez <logan@henriquez.net>
2011-05-25 10:48:57 -0700

    added config stuff to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,10 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 MarSprintDemo::Application.initialize!
 
+MarSprintDemo::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+end  
+
+
+
