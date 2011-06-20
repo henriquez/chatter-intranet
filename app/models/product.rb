3d0e25908d2e4dd86573b812942dd5403ac50399
logan henriquez <logan@henriquez.net>
2011-06-20 00:00:04 -0700

    needs some styling, but basically two columns are there and working

---
+++ app/models/product.rb
@@ -0,0 +1,12 @@
+class Product
+  
+  # Return an hash of products where keys are names and values as the id
+  def self.list
+    { "Model 34-9, Vente Engine" => 1,
+      "Model 54-2, Large Engine" => 2,
+      "Model 23-A, Small Engine" => 3
+    }
+      
+  end
+  
+end
