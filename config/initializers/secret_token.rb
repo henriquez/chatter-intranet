7e5aa1b2a8d9c582200a5729de28f4dcd6e35b15
Logan <logan@henriquez.net>
2013-09-20 15:00:07 -0700

    remove the auto generated secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MarSprintDemo::Application.config.secret_token = '40f528ff1a03235a31d2acd21919be42f2917145f62719bcc1f0da8a944b14eb8764e4f6c831d8083813d7bdf706db8bac00e261574a73d5d46dbcd4297f6e86'
+MarSprintDemo::Application.config.secret_token = 'your secret here'
